136a9e05f64505a0e58fc28053a89fc573d91a8a
cristhian2019 <cristhian@idp.com>
2020-01-12 21:44:39 -0600

    Metodo para llenar cuadro

--- Cuadrado.java
+++ Cuadrado.java
@@ -22,4 +22,33 @@ public class Cuadrado{
         }
     }
 
+    public boolean llenarCuadrado(int f,int c,int valor){
+        if(this.cuadro[f][c] == 0){
+            if(!this.existe(valor)){
+                this.cuadro[f][c] = valor;
+                return true;
+            }
+            else{
+                System.out.println("El valor ya existe en el cuadro o esta ocupada la posición");
+                return false;
+            }
+        }
+        else{ 
+            System.out.println("La casilla esta ocupada");
+            return false; 
+        }
+    }
+
+    public boolean existe(int n){
+        boolean variable = false;
+            for(int i=0; i<=this.cuadro.length-1; i++){
+                for(int j=0; j<=this.cuadro[i].length-1; j++){
+                    if(this.cuadro[i][j] == n){
+                        variable = true;
+                    }
+                }
+            }
+        return variable;
+    }
+
 }
