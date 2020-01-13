7d4ba73829bebcc6639d234f3b65a53005748c6d
cristhian2019 <cristhian@idp.com>
2020-01-12 22:00:26 -0600

    Metodo para verificar victoria

--- Cuadrado.java
+++ Cuadrado.java
@@ -51,4 +51,44 @@ public class Cuadrado{
         return variable;
     }
 
+    public boolean Verificar(){
+        ArrayList<Integer> lista = new ArrayList<Integer>();
+        lista = sumar();
+        int n =lista.get(0);
+        boolean var = true;
+            for(int i=1; i<=lista.size()-1; i++){
+                if(n != lista.get(i)){
+                    var = false;
+                }
+                n = lista.get(i);
+            }
+        return var;
+    }
+     
+    
+    private ArrayList<Integer> sumar(){
+        int suma2=0,suma=0,suma3=0,suma4=0;
+        int d = this.cuadro.length-1;
+        ArrayList<Integer> lista = new ArrayList<Integer>();
+        for(int i=0; i<this.cuadro.length; i++){
+            suma2 = suma2 + this.cuadro[i][d];
+            d--;
+            for(int j=0; j<this.cuadro[i].length; j++)
+            {
+                if(i==j){
+                    suma = suma + this.cuadro[i][j];
+                }
+                suma3 = suma3 + this.cuadro[i][j];
+                suma4 = suma4 + this.cuadro[j][i];
+            }
+            lista.add(suma3);
+            lista.add(suma4);
+            suma4 =0;
+            suma3=0;
+        }
+        lista.add(suma);
+        lista.add(suma2);
+        return lista;
+    }
+
 }
